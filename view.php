befed5fadb319a45b0a49abe883095c51bfe4d60
Lisa Baetsle <lisa_baetsle@hotmail.com>
2020-09-29 16:57:53 +0200

    add styling and reverse the array

--- view.php
+++ view.php
@@ -16,16 +16,20 @@
     <div class="container-form grid-item">
       <h2>Write something in our guestbook</h2>
       <form method="post">
-        Title: <input type="text" name="title" value="">
+        <label for="title">Title: </label>
+        <input type="text" name="title" value="">
         <span class="error">* <?= $titleErr; ?> </span>
         <br><br>
-        Date: <input type="date" name="date" value="">
+        <label for="date">Date: </label>
+        <input type="date" name="date" value="">
         <span class="error">* <?php echo $dateErr; ?> </span>
         <br><br>
-        Content: <textarea name="content" rows="5" cols="40"></textarea>
+        <label for="content">Content: </label>
+        <textarea name="content" rows="5" cols="40"></textarea>
         <span class="error">* <?php echo $contentErr; ?> </span>
         <br><br>
-        Name: <input type="text" name="name" value="">
+        <label for="name">Name: </label>
+        <input type="text" name="name" value="">
         <span class="error">* <?php echo $nameErr; ?> </span>
         <br><br>
         <input type="submit" name="submit" value="Submit">
@@ -35,11 +39,12 @@
     <div class="container-posts grid-item">
       <h2>Previous posts</h2>
       <div class="previous-posts">
-        <?php foreach ($guestbook->getListOfPosts() as $post) {
+        <?php $reversed = array_reverse($guestbook->getListOfPosts());
+        foreach ($reversed as $post) {
           echo '<div class="previous-post"><p class="title">' . $post->getTitle() . '</p><br>';
           echo '<p class="date">' . $post->getDate() . '</p><br>';
           echo '<p class="content">' . $post->getContent() . '</p><br>';
-          echo '<p class="name">' . $post->getName() . '</p><br></div>';
+          echo '<p class="name"> Written by: ' . $post->getName() . '</p><br></div>';
           echo '<br>';
         } ?>
       </div>
