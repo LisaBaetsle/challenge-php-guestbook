919a1386b407d95f8987208d83ab9243489aa258
Lisa Baetsle <lisa_baetsle@hotmail.com>
2020-09-29 16:23:24 +0200

    add layout to the form

--- view.php
+++ view.php
@@ -4,39 +4,45 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
   <title>PHP guestbook</title>
 </head>
 
 <body>
-  <div class="container-form">
+  <header>
     <h1>WELCOME TO OUR GUESTBOOK</h1>
-    <form method="post">
-      Title: <input type="text" name="title" value="">
-      <span class="error">* <?= $titleErr; ?> </span>
-      <br><br>
-      Date: <input type="date" name="date" value="">
-      <span class="error">* <?php echo $dateErr; ?> </span>
-      <br><br>
-      Content: <textarea name="content" rows="5" cols="40"></textarea>
-      <span class="error">* <?php echo $contentErr; ?> </span>
-      <br><br>
-      Name: <input type="text" name="name" value="">
-      <span class="error">* <?php echo $nameErr; ?> </span>
-      <br><br>
-      <input type="submit" name="submit" value="Submit">
-    </form>
-  </div>
+  </header>
+  <div class="grid-container">
+    <div class="container-form grid-item">
+      <h2>Write something in our guestbook</h2>
+      <form method="post">
+        Title: <input type="text" name="title" value="">
+        <span class="error">* <?= $titleErr; ?> </span>
+        <br><br>
+        Date: <input type="date" name="date" value="">
+        <span class="error">* <?php echo $dateErr; ?> </span>
+        <br><br>
+        Content: <textarea name="content" rows="5" cols="40"></textarea>
+        <span class="error">* <?php echo $contentErr; ?> </span>
+        <br><br>
+        Name: <input type="text" name="name" value="">
+        <span class="error">* <?php echo $nameErr; ?> </span>
+        <br><br>
+        <input type="submit" name="submit" value="Submit">
+      </form>
+    </div>
 
-  <div class="container-posts">
-    <h2>Previous posts</h2>
-    <div class="previous-posts">
-      <?php foreach ($guestbook->getListOfPosts() as $post) {
-        echo '<div><p class="title">' . $post->getTitle() . '</p><br>';
-        echo '<p class="date">' . $post->getDate() . '</p><br>';
-        echo '<p class="content">' . $post->getContent() . '</p><br>';
-        echo '<p class="name">' . $post->getName() . '</p><br></div>';
-        echo '<br>';
-      } ?>
+    <div class="container-posts grid-item">
+      <h2>Previous posts</h2>
+      <div class="previous-posts">
+        <?php foreach ($guestbook->getListOfPosts() as $post) {
+          echo '<div class="previous-post"><p class="title">' . $post->getTitle() . '</p><br>';
+          echo '<p class="date">' . $post->getDate() . '</p><br>';
+          echo '<p class="content">' . $post->getContent() . '</p><br>';
+          echo '<p class="name">' . $post->getName() . '</p><br></div>';
+          echo '<br>';
+        } ?>
+      </div>
     </div>
   </div>
 
